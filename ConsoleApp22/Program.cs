0eacd962814e7cc7458b233010caba80d5efb258
劉敦桐 <zse811022@hotmail.com>
2023-09-06 21:34:08 +0800

    DAY5

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+
+
+			int firstNumber = numbers[0];  // firstNumber = 1
+
+			numbers[0] = 6;
+
+			foreach (var item in numbers)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
